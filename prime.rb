d5c33a5d734edcfa9f2b0de0e18e3d5b6ce02f86
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-05 00:20:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(integer)
+# test every number less than integer
+# to see if it's a factor
+  if integer <= 1
+    return false
+#  elsif integer % 4 == 0
+#    return false
+  elsif integer == 2||3
+    return true
+  else
+    testarray = (3..integer).to_a
+    testarray.each do |element|
+      if integer % element == 0
+        return false
+      else
+        return true
+      end
+    end
+  end
+end
+
+#what if we start with integer-1 and move down to 0?
+#not sure why it's returning true for 40 % 4
